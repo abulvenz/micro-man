08cd57e0668a0d59623d9d2782aacb5f143df6dd
abulvenz <abulvenz@protonmail.ch>
2024-10-08 08:29:09 +0200

    feat: Initial commit. Pipeline, stubs.

---
+++ index.mjs
@@ -0,0 +1,102 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+import * as jsonpatch from "fast-json-patch/index.mjs";
+import { auth } from "express-openid-connect";
+// import persistence from "./persistence.mjs";
+
+import { env } from 'custom-env';
+env();
+
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+if (process.env.SECRET)
+  app.use(
+    auth({
+      authRequired: true,
+      idpLogout: true,
+      routes: {
+        login: "/login",
+        logout: "/logout",
+        postLogoutRedirect: "http://localhost:4000",
+      },
+    })
+  );
+
+
+app.get("/", (req, res) => {
+  if (req.oidc)
+    res.cookie("session", req.oidc.user.sub);
+  res.sendFile(join(__dirname, "dist", "index.html"));
+});
+
+app.use(express.static("dist"));
+
+const range = (N) => {
+  const r = [];
+  for (let i = 0; i < N; i++) {
+    r.push(i);
+  }
+  return r;
+};
+
+const randomInt = (N) => Math.trunc(Math.random() * N);
+
+const use = (v, f) => f(v);
+
+const shuffle = (arr, r = []) =>
+  use(
+    arr.map((e) => e),
+    (a) => range(arr.length).map((i) => a.splice(randomInt(a.length), 1)[0])
+  );
+
+const users = [];
+
+io.on("connection", (socket) => {
+  socket.on("i am", async (msg) => {
+    console.log("i am", msg);
+    const useridx = msg.id;
+    if (users.findIndex((u) => u.idx === useridx) < 0)
+      users.push({ socket, idx: useridx });
+    io.emit("hi", { id: useridx, users: users.map((u) => u.idx) });
+    console.log(
+      "a user connected",
+      users.map((u) => u.idx)
+    );
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+      users.splice(
+        users.findIndex((e) => e.idx === useridx),
+        1
+      );
+      io.emit("goodbye", { id: useridx, users: users.map((u) => u.idx) });
+    });
+    socket.on("chat message", (msg) => {
+      io.emit("chat message", msg);
+    });
+
+    socket.on("game", async (msg) => {
+      const gameid = `game-${useridx}-${msg.user}`;
+      console.log("creating game", gameid);
+      socket.join(gameid);
+      users.find((u) => u.idx === msg.user).socket.join(gameid);
+
+      //      io.to(gameid).emit("field", game.pop());
+    });
+
+  });
+
+});
+
+server.listen(4000, () => {
+  console.log("server running at http://localhost:4000");
+});
---
+++ main.js
@@ -0,0 +1,144 @@
+import m from "mithril";
+import tagl from "tagl-mithril";
+import io from "socket.io/client-dist/socket.io";
+const { trunc } = Math;
+const { div, ul, li, h1, h3, form, input, button, table, tr, td, pre } =
+  tagl(m);
+const messages = [];
+const use = (v, f) => f(v);
+
+
+const socket = io();
+
+const range = (N) => {
+  const r = [];
+  for (let i = 0; i < N; i++) {
+    r.push(i);
+  }
+  return r;
+};
+
+socket.on("connect", () => {
+  const userID =
+    localStorage.getItem("id") || localStorage.setItem("id", socket.id);
+  socket.emit("i am", { id: userID });
+});
+
+let users = [];
+let ownid = undefined;
+
+const connectEvent = (eventName, callback) =>
+  socket.on(eventName, (p) => {
+    callback(p);
+    m.redraw();
+  });
+
+connectEvent("hi", (msg) => {
+  messages.push({ sender: "OPERATOR", msg: "Welcome " + msg.id });
+  users = msg.users;
+  ownid = ownid || msg.id;
+  games = msg.games || [];
+  console.log(ownid, users);
+});
+
+connectEvent("chat message", (msg) => {
+  messages.push(msg);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+connectEvent("field", (msg) => {
+  game = msg;
+});
+
+connectEvent("games", (msg) => {
+  games = msg;
+});
+
+const state = {
+  msg: "",
+};
+
+const mygame = () => (game && game.playerB === ownid) || game.playerW === ownid;
+
+const send = () => {
+  socket.emit("chat message", { sender: socket.id, msg: state.msg });
+  state.msg = "";
+};
+
+const enterGame = (gameid) => {
+  socket.emit("enter game", { gameid });
+};
+
+const fcol = (v) => `f${figures[v].color}`;
+
+const fieldClass = (idx) =>
+  (idx + trunc(idx / 8)) % 2 === 0 ? "black" : "white";
+
+const isSelected = (idx) => idx === game.selected;
+const isValid = (idx) => game.valid.indexOf(idx) >= 0;
+const isCheck = (idx) => game.check === idx;
+
+const select = (idx) => {
+  socket.emit("select", { selected: idx, gameid: game.id });
+};
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+}
+
+// Swap the field if the player is white
+const it = (cb) =>
+  use(game.playerW === ownid ? (i) => 63 - i : (i) => i, (map) =>
+    range(64)
+      .map(map)
+      .map((i) => cb(game.field[i], i))
+  );
+
+const userListC = (vnode) => ({
+  view: (vnode) =>
+    ul(
+      users.map((u) =>
+        li(
+          u,
+          u === ownid ? " (This is you)" : null,
+          button(
+            { onclick: () => socket.emit("game", { user: u }) },
+            "Challenge"
+          )
+
+        )
+      )
+    ),
+});
+
+const chatC = (vnode) => ({
+  view: (vnode) => [
+    ul.$messages(messages.map((msg) => li(msg.sender + ": " + msg.msg))),
+    div.$form(
+      input.$input({
+        value: state.msg,
+        oninput: (e) => (state.msg = e.target.value),
+        onkeydown: (e) => {
+          if (e.key === "Enter") {
+            send();
+          }
+        },
+        autocomplete: "off",
+      }),
+      button({ onclick: (e) => send() }, "Send")
+    ),
+  ],
+});
+
+
+m.mount(document.body, {
+  view: (vnode) => [
+    [m(userListC), m(chatC)],
+    button({ onclick: toggleFullScreen }, "Toggle Fullscreen"),
+    // pre(ownid + "\n" + JSON.stringify(game, null, 2)),
+  ],
+});
